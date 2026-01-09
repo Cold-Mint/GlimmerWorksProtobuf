943cee5187f00bc02e5a2d116af7ba8acd172053
Cold-Mint <cold_mint@qq.com>
2026-01-09 16:19:54 +0800

    Update CMakeLists.txt

---
+++ versions.cmake
@@ -0,0 +1,4 @@
+#Variable definitions are generated through GlimmerWorksBuilder. Please do not edit them.
+#The dependencies of the repository can be defined through comments.
+#protocolbuffers/protobuf@latest
+set(PROTOCOLBUFFERS_PROTOBUF_VERSION "v33.2")
