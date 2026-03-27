d1853ea8e6f40c579dc9376ea1e9adda62c22623
Cold-Mint <cold_mint@qq.com>
2026-03-27 11:51:55 +0800

    Change to Lite.

--- versions.cmake
+++ versions.cmake
@@ -1,4 +1,4 @@
 #Variable definitions are generated through GlimmerWorksBuilder. Please do not edit them.
 #The dependencies of the repository can be defined through comments.
 #protocolbuffers/protobuf@latest
-set(PROTOCOLBUFFERS_PROTOBUF_VERSION "v34.0")
+set(PROTOCOLBUFFERS_PROTOBUF_VERSION "v34.1")
