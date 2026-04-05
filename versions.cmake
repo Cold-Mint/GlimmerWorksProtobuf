cdcd54340438d3c0ed014dd94d46bbff6173635e
Cold-Mint <cold_mint@qq.com>
2026-04-05 09:04:50 +0800

    Use the version configuration file of the root directory.

--- versions.cmake
+++
@@ -1,4 +0,0 @@
-#Variable definitions are generated through GlimmerWorksBuilder. Please do not edit them.
-#The dependencies of the repository can be defined through comments.
-#protocolbuffers/protobuf@latest
-set(PROTOCOLBUFFERS_PROTOBUF_VERSION "v34.1")
